b744812125a2595a7e87066d0e71e89f9e528323
Korchemkin <sergey.korchemkin@dxc.com>
2019-12-05 22:13:37 +0300

    Set up React environment w/o CRA

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Header from './layout/Header';
+// import Footer from './layout/Footer';
+// import Main from './layout/Main';
+// import App from 'componenents/app/app';
+
+const App = (props) =>
+  (<div>
+    <header></header>
+    <main></main>
+    <footer></footer>
+  </div>);
+
+ReactDOM.render(<App />, document.getElementById('app'));
